de865a4abb8cf122cb0bb5a1cff20ea039baa7dc
Haojun Sui <shjnyr@me.com>
2015-05-27 12:49:06 +0800

    Update HierarchicalClusteringPanel.java

--- HierarchicalClusteringPanel.java
+++ HierarchicalClusteringPanel.java
@@ -1,6 +1,9 @@
-
+import java.awt.BorderLayout;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class HierarchicalClusteringPanel extends JPanel
@@ -12,35 +15,10 @@ public class HierarchicalClusteringPanel extends JPanel
             clusters.add(new Cluster(p));
         
         
-        int temp = 0;
-        
-        while (temp < 9 && clusters.size() != 1)
-        {
-            Cluster A = clusters.get(0);
-            Cluster B = clusters.get(0);
-            double dis = 1000000000, temp_dis;
-            for (int i = 0; i < clusters.size(); i++)
-                for (int j = 0; j < clusters.size(); j++)
-                {
-                    if (j != i)
-                    {
-                        temp_dis = Math.sqrt(Math.pow(clusters.get(i).centroid.x() - clusters.get(j).centroid.x(), 2) + Math.pow(clusters.get(i).centroid.y() - clusters.get(j).centroid.y(), 2));
-                        if (temp_dis < dis)
-                        {
-                            dis = temp_dis;
-                            A = clusters.get(i);
-                            B = clusters.get(j);
-                        }
-                    }
-                }
-            
-            clusters.remove(A);
-            clusters.remove(B);
-            
-            clusters.add(new Cluster(A, B));
-            temp++;
-            repaint();
-        }
+        setLayout(new BorderLayout());
+        JButton j = new JButton("Next");
+        add(j, BorderLayout.SOUTH);
+        j.addActionListener(new nextButtonHandler());
     }
     
     @Override
@@ -48,5 +26,39 @@ public class HierarchicalClusteringPanel extends JPanel
     {
         for (Cluster c : clusters)
             c.paintComponent(g);
+    }
+    
+    private class nextButtonHandler implements ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+            if (clusters.size() != 1)
+            {
+                Cluster A = clusters.get(0);
+                Cluster B = clusters.get(0);
+                double dis = 1000000000, temp_dis;
+                for (int i = 0; i < clusters.size(); i++)
+                    for (int j = 0; j < clusters.size(); j++)
+                    {
+                        if (j != i)
+                        {
+                            temp_dis = Math.sqrt(Math.pow(clusters.get(i).centroid.x() - clusters.get(j).centroid.x(), 2) + Math.pow(clusters.get(i).centroid.y() - clusters.get(j).centroid.y(), 2));
+                            if (temp_dis < dis)
+                            {
+                                dis = temp_dis;
+                                A = clusters.get(i);
+                                B = clusters.get(j);
+                            }
+                        }
+                    }
+
+                clusters.remove(A);
+                clusters.remove(B);
+
+                clusters.add(new Cluster(A, B));
+                repaint();
+            }
+        }
     }
 }
